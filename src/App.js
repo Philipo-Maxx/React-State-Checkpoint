8b399945c7bd1f5e98a347e435d9a64b4361ecb3
Philipo Maxx <philip.maxx06@gmail.com>
2024-10-03 10:33:02 +0100

    react state checkpoint done using react bootstrap card and class based component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// Importing bootstrap components
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Using class component
+class App extends React.Component {
+  // Initial state
+  state = {
+    fullName: "Bukayo Saka",
+    bio: "Male",
+    imgSrc:
+      "https://www.arsenal.com/sites/default/files/styles/large_16x9/public/images/Saka_46.jpg?h=74c28fda&auto=webp&itok=LovYzw79",
+    profession: "Footballer",
+    shows: true,
+    timeMounted: 0,
+    timeElapsed: 0,
+  };
+
+  // Component lifecycle
+  componentDidMount() {
+    this.setState({ timeMounted: Date.now() });
+
+    // setInterval to update timeElapsed every
+    this.timer = setInterval(() => {
+      this.updateShow();
+    }, 1000);
+  }
+
+  // Function to update timeElapsed
+  updateShow() {
+    const { timeMounted } = this.state;
+    // Calculate time elapsed since mounting
+    const updatedTime = Date.now();
+    // Convert time to seconds
+    const elapsedTime = Math.floor((updatedTime - timeMounted) / 1000);
+    // Update timeElapsed
+    this.setState({ timeElapsed: elapsedTime });
+  }
+
+  // Clear interval
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  // Function to handle button click
+  handleClick = () => {
+    this.setState({ shows: !this.state.shows });
+  };
+
+  // Render method
+  render() {
+    let text = "";
+    // Change button text based on state
+    text = this.state.shows ? "Hide Profile" : "Show Profile";
+    return (
+      <div className="text-center ">
+        <Container>
+          <Row>
+            {this.state.shows ? (
+              <Col sm={4} className="m-auto mt-5">
+                <Card style={{}}>
+                  <Card.Img variant="top" src={this.state.imgSrc} />
+                  <div
+                    className="bg-light text-center"
+                    style={{ fontSize: "20px", fontStyle: "italic" }}
+                  >
+                    <Card.Body>
+                      <Card.Title>{this.state.fullName}</Card.Title>
+                      <Card.Text>{`Bio: ${this.state.bio}`}</Card.Text>
+                      <div className="text-center">
+                        <Card.Text className="d-inline-block">{`Profession: ${this.state.profession}`}</Card.Text>
+                      </div>
+                    </Card.Body>
+                  </div>
+                </Card>
+              </Col>
+            ) : (
+              <Col sm={4} className="d-none"></Col>
+            )}
+
+            <div className="text-center mt-5">
+              <Button variant="primary" onClick={this.handleClick}>
+                {text}
+              </Button>
+            </div>
+            <p>Time Elapsed Since Mounting: {this.state.timeElapsed} seconds</p>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
+//  Export App component
 export default App;
